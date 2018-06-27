6e55fdfc584a4e09944d97bcabcf509682ec2d32
Will Reese <willbo118@gmail.com>
2018-06-26 17:42:14 -0700

    Removing old code

--- spawn.manager.js
+++ spawn.manager.js
@@ -1,32 +1,10 @@
-var roleHarvester = require('creep.role.harvester');
-var roleUpgrader = require('creep.role.upgrader');
-var roleBuilder = require('creep.role.builder');
-
 module.exports = {
   run: function() {
-    //TODO: Multiple Spawns
-
-    var spawn;
+    
+    let spawn;
     for (var prop in Game.spawns) {
       spawn = Game.spawns[prop];
       break;
-    }
-
-    var creepRoleCounts = {};
-    creepRoleCounts[roleHarvester.name] = 0;
-    creepRoleCounts[roleUpgrader.name] = 0;
-    creepRoleCounts[roleBuilder.name] = 0;
-
-    for (var name in Game.creeps) {
-      var creep = Game.creeps[name];
-      creepRoleCounts[creep.memory.role] =
-        creepRoleCounts[creep.memory.role] + 1;
-      var roomMemory = creep.room.memory;
-      if (!roomMemory.hasOwnProperty('creepCount')) {
-        roomMemory.creepCount = 1;
-      } else {
-        roomMemory.creepCount = roomMemory.creepCount + 1;
-      }
     }
 
     Memory.debug = {};
@@ -38,19 +16,9 @@ module.exports = {
         room.memory.miningPositions.length * 1.5,
       );
       if (creepRoleCounts[roleHarvester.name] < Math.floor(targetCreepCount)) {
-        var newName = roleHarvester.name + Game.time;
-        spawn.spawnCreep([WORK, CARRY, MOVE], newName, {
-          memory: {role: roleHarvester.name},
-        });
-      } else if (
-        creepRoleCounts[roleUpgrader.name] < Math.floor(targetCreepCount * 0.25)
-      ) {
-        var newName = roleUpgrader.name + Game.time;
-        spawn.spawnCreep([WORK, CARRY, MOVE], newName, {memory: { role: roleUpgrader.name }});
-      } else if (creepRoleCounts[roleBuilder.name] < 1) {
-        var newName = roleBuilder.name + Game.time;
-        spawn.spawnCreep([WORK, CARRY, MOVE], newName, {memory: { role: roleBuilder.name }});
-      }
+        var newName = "Creep_" + Game.time;
+        spawn.spawnCreep([WORK, CARRY, MOVE], newName);
+      }     
     }
   },
 };
