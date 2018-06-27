195fedb36ea8015e87330da9df6fbefba9530f8b
Will Reese <willbo118@gmail.com>
2018-06-27 15:45:41 -0700

    Updating room manager to use es6 classes and fixing bug
    Updating room manager to be static
    
    Fixing issue when there are no enemies in the room
    
    Fixing issue when there are no enemies in the room

--- room.manager.js
+++ room.manager.js
@@ -1,11 +1,9 @@
-module.exports = {
-  /** @param Room room**/
-  run: function(room) {
+class RoomManager {
+  static exec(room) {
     const UPDATE_EVERY = 100;
 
-    //Find all valid energy mining spots
-    var memory = room.memory;
-    var needsUpdate;
+    let memory = room.memory;
+    let needsUpdate;
     if (memory.hasOwnProperty('needsUpdateAt')) {
       needsUpdate = memory.needsUpdateAt < Game.time;
     } else {
@@ -16,36 +14,48 @@ module.exports = {
     if (needsUpdate) {
       memory.needsUpdateAt = Game.time + UPDATE_EVERY;
       if (memory.miningPositions == null) {
-        memory.miningPositions = [];
-        var sources = room.find(FIND_SOURCES);
-        for (var index in sources) {
-          var source = sources[index];
-          var pos = source.pos;
-          for (var i = pos.x - 1; i <= pos.x + 1; i++) {
-            for (var j = pos.y - 1; j <= pos.y + 1; j++) {
-              var entities = room.lookAt(i, j);
-              for (var z in entities) {
-                var entity = entities[z];
-                if (entity.type == 'terrain' && entity.terrain == 'plain') {
-                  memory.miningPositions.push({
-                    pos: pos,
-                    source: source.id,
-                    safe: false,
-                  });
-                }
-              }
+        RoomManager.findMiningPos(room);
+      }
+
+      RoomManager.checkMiningPosSafety(room);
+    }
+  }
+
+  static findMiningPos(room) {
+    let memory = room.memory;
+    memory.miningPositions = [];
+    let sources = room.find(FIND_SOURCES);
+    for (let index in sources) {
+      var source = sources[index];
+      var pos = source.pos;
+      for (let i = pos.x - 1; i <= pos.x + 1; i++) {
+        for (let j = pos.y - 1; j <= pos.y + 1; j++) {
+          var entities = room.lookAt(i, j);
+          for (let z in entities) {
+            var entity = entities[z];
+            if (entity.type == 'terrain' && entity.terrain == 'plain') {
+              memory.miningPositions.push({
+                pos: pos,
+                source: source.id,
+                safe: false,
+              });
             }
           }
         }
       }
-
-      //Check if mining positions are safe
-      for (var i = 0; i < memory.miningPositions.length; i++) {
-        let coords = memory.miningPositions[i].pos;
-        let pos = memory.getPositionAt(coords.x, coords.y);
-        let hostile = pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        room.safe = !hostile.pos.inRangeTo(pos);
-      }
     }
-  },
-};
+  }
+
+  static checkMiningPosSafety(room) {
+    let memory = room.memory;
+    //Check if mining positions are safe
+    for (var i = 0; i < memory.miningPositions.length; i++) {
+      let coords = memory.miningPositions[i].pos;
+      let pos = room.getPositionAt(coords.x, coords.y);
+      let hostile = pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+      memory.miningPositions[i].safe = hotile == null || !hostile.pos.inRangeTo(pos);
+    }
+  }
+}
+
+module.exports = RoomManager;
