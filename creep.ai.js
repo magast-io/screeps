adbc4223b6ecbd038aaf45b1610cdc1c190960f2
Will Reese <willbo118@gmail.com>
2018-06-26 17:22:49 -0700

    Adding creep.ai.js file

---
+++ creep.ai.js
@@ -0,0 +1,33 @@
+var actions = require('creep.actions');
+
+export class CreepAI {
+  constructor() {
+    this.actions = {};
+    this.actions[HarvestEnergy.name] = new HarvestEnergy();
+    this.actions[DropoffEnergy.name] = new DropoffEnergy();
+    this.actions[UpgradeController.name] = new UpgradeController();
+    this.actions[Retreat.name] = new Retreat();
+  }
+
+  exec(creep) {
+    let current = this.actions[creep.memory.action];
+    let scores = [];
+    for (let key in this.actions) {
+      let action = this.actions[key];
+      scores.push({name: key, score: action.score(creep)});
+    }
+
+    scores.sort(function(a, b) {
+      return a.score > b.score;
+    });
+
+    let next = scores[0];
+
+    if (current != next) {
+      current.finish(creep);
+      next.setup(creep);
+    }
+
+    next.exec(creep);
+  }
+}
